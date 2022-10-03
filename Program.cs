07e7d7d9034202436446320addcce780eb12dcee
vishal alok <valok18@gmail.com>
2022-10-04 01:08:34 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿bool[] a =new bool[100];
+for (int i = 1; i <= 100; i++)
+{
+    a[i-1] = false;
+}
+//for (int i = 1; i <= 100; i++)
+//{
+//    Console.WriteLine(i + " is " + a[i-1]);
+//}
+for (int i = 1; i <= 100; i++)
+{
+    for (int j = 1; j <= 100; j++)
+    {
+        if(((i * j) - 1)<=100)
+        a[(i * j) - 1] = !a[(i * j) - 1];
+    }
+
+}
+
+for (int k = 1; k <= 100; k++)
+{
+    Console.WriteLine(k + " is " + a[k - 1]);
+}
